438f9abf38915a4e6a4f7d78361634da236c6a57
Jinyoung Jang <jyjang@uengine.org>
2024-05-01 11:51:33 +0900

    commit

---
+++ src/main/java/metering/domain/Value.java
@@ -0,0 +1,24 @@
+package metering.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+//<<< DDD / Value Object
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Value {
+
+    private String key;
+
+    private String value;
+
+    private Date activateFrom;
+}
+//>>> DDD / Value Object
