a6d353ed33595c351461ffae9f58481ea1d1d6b5
Jinyoung Jang <jyjang@uengine.org>
2024-05-01 12:07:39 +0900

    commit

---
+++ src/main/java/domain/CoefficientRepository.java
@@ -0,0 +1,13 @@
+package metering.domain;
+
+import metering.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//<<< PoEAA / Repository
+@RepositoryRestResource(
+    collectionResourceRel = "coefficients",
+    path = "coefficients"
+)
+public interface CoefficientRepository
+    extends PagingAndSortingRepository<Coefficient, Long> {}
---
+++ src/main/java/infra/CoefficientController.java
@@ -0,0 +1,23 @@
+package metering.infra;
+
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import metering.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//<<< Clean Arch / Inbound Adaptor
+
+@RestController
+// @RequestMapping(value="/coefficients")
+@Transactional
+public class CoefficientController {
+
+    @Autowired
+    CoefficientRepository coefficientRepository;
+}
+//>>> Clean Arch / Inbound Adaptor
